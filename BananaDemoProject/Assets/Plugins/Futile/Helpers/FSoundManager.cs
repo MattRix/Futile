dfaedc8cd4bc51ac4a3894904e64a1e844e48b8e
Matt Rix <matt@matt-rix.com>
2012-08-17 13:36:38 -0400

    Fixing FSoundManager blowup on scene change
    This solves this issue:
    http://www.reddit.com/r/futile/comments/yc8f4/bug_report_fsoundmanager/

--- BananaDemoProject/Assets/Plugins/Futile/Helpers/FSoundManager.cs
+++ BananaDemoProject/Assets/Plugins/Futile/Helpers/FSoundManager.cs
@@ -2,32 +2,26 @@ using System;
 using UnityEngine;
 using System.Collections.Generic;
 
-
 public class FSoundManager
 {
-	static private String _resourcePrefix;
+	static private String _resourcePrefix = "Audio/";
 	
 	static private GameObject _gameObject;
 	static private AudioListener _listener;
 	static private AudioSource _soundSource;
 	static private AudioSource _musicSource;
 	
-	static private Dictionary<string, AudioClip> _soundClips;
-	private static AudioClip _currentMusicClip;
+	static private Dictionary<string, AudioClip> _soundClips = new Dictionary<string, AudioClip>();
+	private static AudioClip _currentMusicClip = null;
 
-	static FSoundManager ()
+	static public void Init()
 	{
 		_gameObject = new GameObject("FSoundManager");
 		_musicSource = _gameObject.AddComponent<AudioSource>();
 		_soundSource = _gameObject.AddComponent<AudioSource>();
-		
 		_gameObject.AddComponent<AudioListener>(); //we don't need a reference to it, we just need it to exist
-		
-		_resourcePrefix = "Audio/";
-		_soundClips = new Dictionary<string, AudioClip>();
-		_currentMusicClip = null;
 	}
-
+	
 	static public void SetResourcePrefix (String prefix)
 	{
 		_resourcePrefix = prefix;
@@ -35,6 +29,8 @@ public class FSoundManager
 
 	static public void PlaySound (String resourceName, float volume)
 	{
+		if(_soundSource == null) Init ();
+		
 		string fullPath = _resourcePrefix+resourceName;
 		
 		AudioClip soundClip;
@@ -59,6 +55,8 @@ public class FSoundManager
 
 	static public void PlayMusic (string resourceName, float volume)
 	{
+		if(_musicSource == null) Init ();
+		
 		string fullPath = _resourcePrefix+resourceName;
 		
 		if(_currentMusicClip != null) //we only want to have one music clip in memory at a time
@@ -99,7 +97,7 @@ public class FSoundManager
 	{
 		UnloadAllSounds();
 		
-		if(_currentMusicClip)
+		if(_currentMusicClip != null)
 		{
 			Resources.UnloadAsset(_currentMusicClip);
 		}
