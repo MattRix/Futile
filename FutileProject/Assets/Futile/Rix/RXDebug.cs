fb24fecdd8204521aa3c53052de17cd3e5a56600
Matt Rix <matt@matt-rix.com>
2013-06-08 02:05:34 -0400

    Make RXDebug.Log() a bit more efficient

--- FutileProject/Assets/Futile/Rix/RXDebug.cs
+++ FutileProject/Assets/Futile/Rix/RXDebug.cs
@@ -6,24 +6,30 @@ using System.Text;
 
 public static class RXDebug
 {
-	static RXDebug()
-	{
-
-	}
-
 	public static void Log(params object[] objects)
 	{
-		StringBuilder builder = new StringBuilder();
-	
-		int count = objects.Length;
+		string result;
 
-		for(int t = 0;t<count;t++)
+		if(objects.Length == 1)
 		{
-			builder.Append(objects[t].ToString());
-			if(t < count-1) builder.Append(',');
+			result = objects[0].ToString();
+		}
+		else 
+		{
+			StringBuilder builder = new StringBuilder();
+		
+			int count = objects.Length;
+
+			for(int t = 0;t<count;t++)
+			{
+				builder.Append(objects[t].ToString());
+				if(t < count-1) builder.Append(',');
+			}
+
+			result = builder.ToString();
 		}
 
-		Debug.Log(builder.ToString());
+		Debug.Log(result);
 	}
 }
 
