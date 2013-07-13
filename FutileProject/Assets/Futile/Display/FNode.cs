5fefd60000840ef16817528870e6bf9cf8fbeb26
Matt Rix <matt@matt-rix.com>
2013-07-13 14:40:27 -0400

    Move node depth relative to another node

--- FutileProject/Assets/Futile/Display/FNode.cs
+++ FutileProject/Assets/Futile/Display/FNode.cs
@@ -405,10 +405,24 @@ public class FNode
 	{
 		if(_container != null) _container.AddChild(this);
 	}
-	
+
 	public void MoveToBack()
 	{
 		if(_container != null) _container.AddChildAtIndex(this,0);	
+	}
+
+	public void MoveInFrontOfOtherNode(FNode otherNode)
+	{
+		if(_container == null) return; //we have no container
+		if(otherNode.container != _container) return; //we don't share a container
+		_container.AddChildAtIndex(this,_container.GetChildIndex(otherNode)+1);
+	}
+
+	public void MoveBehindOtherNode(FNode otherNode)
+	{
+		if(_container == null) return; //we have no container
+		if(otherNode.container != _container) return; //we don't share a container
+		_container.AddChildAtIndex(this,_container.GetChildIndex(otherNode));
 	}
 
 	public bool isVisible
