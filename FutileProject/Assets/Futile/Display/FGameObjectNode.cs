74c030e815c9cab4f1fe5250b48fe2812add0623
Matt Rix <matt@matt-rix.com>
2013-09-14 14:39:23 -0400

    Make FGameObjectNode use prefab scale+rotation
    FGameObjectNode would previously just ignore the rotation and scale of
    the gameobject/prefab when initing it, but now it remembers it. This was
    suggested in pull request #188 by @wtrebella

--- FutileProject/Assets/Futile/Display/FGameObjectNode.cs
+++ FutileProject/Assets/Futile/Display/FGameObjectNode.cs
@@ -4,11 +4,17 @@ using System;
 public class FGameObjectNode : FNode, FRenderableLayerInterface
 {
 	protected GameObject _gameObject;
+
+	protected int _renderQueueDepth = -1;
+
 	protected bool _shouldLinkPosition;
 	protected bool _shouldLinkRotation;
 	protected bool _shouldLinkScale;
-	protected int _renderQueueDepth = -1;
-	
+
+	protected Vector3 _initialGameObjectScale = new Vector3(1.0f,1.0f,1.0f);
+	protected float _initialGameObjectRotationZ = 0.0f;
+	protected float _previousGameObjectRotationZ = 0.0f;
+
 	public bool shouldDestroyOnRemoveFromStage = true;
 	
 	public FGameObjectNode (GameObject gameObject, bool shouldLinkPosition, bool shouldLinkRotation, bool shouldLinkScale)
@@ -27,6 +33,10 @@ public class FGameObjectNode : FNode, FRenderableLayerInterface
 		_shouldLinkPosition = shouldLinkPosition;
 		_shouldLinkRotation = shouldLinkRotation;
 		_shouldLinkScale = shouldLinkScale;
+
+		_initialGameObjectScale = gameObject.transform.localScale;
+		_initialGameObjectRotationZ = gameObject.transform.rotation.eulerAngles.z;
+		_previousGameObjectRotationZ = _initialGameObjectRotationZ;
 		
 		Setup();
 	}
@@ -43,6 +53,8 @@ public class FGameObjectNode : FNode, FRenderableLayerInterface
 			}
 			
 			_gameObject.layer = _stage.layer;
+			
+			UpdateGameObject();
 		}
 	}
 	
@@ -140,15 +152,29 @@ public class FGameObjectNode : FNode, FRenderableLayerInterface
 		{
 			//TODO: Get these values correctly using the full matrix
 			//do it with scale too
-			
-			//need to use the FULL global matrix
-			
 			FMatrix matrix = this.screenConcatenatedMatrix;
 			
 			if(_shouldLinkPosition) _gameObject.transform.localPosition = matrix.GetVector3FromLocalVector2(Vector2.zero,0);
-			if(_shouldLinkRotation) _gameObject.transform.eulerAngles = new Vector3(_gameObject.transform.eulerAngles.x,_gameObject.transform.eulerAngles.y,matrix.GetRotation());
-			if(_shouldLinkScale) _gameObject.transform.localScale = new Vector3(matrix.GetScaleX(), matrix.GetScaleY (), _gameObject.transform.localScale.z);
-			
+			if(_shouldLinkRotation)
+			{	
+				float newRotation = matrix.GetRotation();
+				//_gameObject.transform.eulerAngles = new Vector3(_gameObject.transform.eulerAngles.x,_gameObject.transform.eulerAngles.y,matrix.GetRotation());
+				_gameObject.transform.Rotate(0,0,_previousGameObjectRotationZ - _initialGameObjectRotationZ - newRotation, Space.World);
+				_previousGameObjectRotationZ = _initialGameObjectRotationZ + newRotation;
+
+			}
+			if(_shouldLinkScale) 
+			{
+				//_gameObject.transform.localScale = new Vector3(matrix.GetScaleX(), matrix.GetScaleY (), _gameObject.transform.localScale.z);
+
+				_gameObject.transform.localScale = new Vector3
+				(
+					_initialGameObjectScale.x * matrix.GetScaleX(), 
+					_initialGameObjectScale.y * matrix.GetScaleY(), 
+					_initialGameObjectScale.z
+				);
+			}
+
 			_gameObject.layer = _stage.layer;
 		}
 	}
