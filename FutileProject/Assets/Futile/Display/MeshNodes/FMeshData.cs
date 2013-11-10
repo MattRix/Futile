4b995ea920fbe6e86d037ee48969ab9bfa6f262a
Matt Rix <matt@matt-rix.com>
2013-11-10 11:53:40 -0500

    Preliminary MeshNode system
    A *MUCH* easier way to create "custom" nodes with dynamic structures.
    This is still considered alpha and is very subject to change.

---
+++ FutileProject/Assets/Futile/Display/MeshNodes/FMeshData.cs
@@ -0,0 +1,438 @@
+
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+public class FMeshData
+{
+	public List<FMeshFacet> facets;
+	public FFacetType facetType;
+	public int version = 0;
+
+	public Action SignalUpdate;
+
+	public FMeshData()
+	{
+		//if you use this constructor, you must call AddFacet/AddQuad/AddTriangle before passing it to the FMeshNode
+	}
+
+	public FMeshData(FFacetType facetType)
+	{
+		this.facets = new List<FMeshFacet>();
+		this.facetType = facetType;
+	}
+
+	public FMeshData(List<FMeshFacet> inFacets)
+	{
+		facets = inFacets;
+
+		if(facets.Count != 0)
+		{
+			facetType = facets[0].facetType;
+		}
+
+	}
+
+	public FMeshData(params FMeshFacet[] inFacets)
+	{
+		facets = new List<FMeshFacet>(inFacets);
+		
+		if(facets.Count != 0)
+		{
+			facetType = facets[0].facetType;
+		}
+	}
+
+	public FMeshFacet AddFacet(FMeshFacet facet)
+	{
+		if(facets == null)
+		{
+			facets = new List<FMeshFacet>();
+			facetType = facet.facetType;
+		}
+
+		if(facetType != facet.facetType) //check if the facet type is different from what we already have
+		{
+			Debug.LogError("You can't mix facet types in FMeshData!");
+		}
+
+		facets.Add(facet);
+
+		return facet;
+	}
+
+	public FMeshTriangle AddTriangle()
+	{
+		FMeshTriangle triangle = new FMeshTriangle();
+		AddFacet(triangle);
+		return triangle;
+	}
+
+	public FMeshTriangle AddTriangle(FMeshTriangle triangle)
+	{
+		AddFacet(triangle);
+		return triangle;
+	}
+
+	public FMeshTriangle AddTriangle(FMeshVertex[] vertices)
+	{
+		FMeshTriangle triangle = new FMeshTriangle(vertices);
+		AddFacet(triangle);
+		return triangle;
+	}
+	
+	public FMeshTriangle AddTriangle(FMeshVertex vertex1, FMeshVertex vertex2, FMeshVertex vertex3)
+	{
+		FMeshTriangle triangle = new FMeshTriangle(vertex1,vertex2,vertex3);
+		AddFacet(triangle);
+		return triangle;
+	}
+
+	public FMeshTriangle GetTriangle(int index)
+	{
+		return facets[index] as FMeshTriangle;
+	}
+
+	public FMeshQuad AddQuad()
+	{
+		FMeshQuad quad = new FMeshQuad();
+		AddFacet(quad);
+		return quad;
+	}
+	
+	public FMeshQuad AddQuad(FMeshQuad quad)
+	{
+		AddFacet(quad);
+		return quad;
+	}
+
+	public FMeshQuad AddQuad(FMeshVertex[] vertices)
+	{
+		FMeshQuad quad = new FMeshQuad(vertices);
+		AddFacet(quad);
+		return quad;
+	}
+	
+	public FMeshQuad AddQuad(FMeshVertex vertex1, FMeshVertex vertex2, FMeshVertex vertex3, FMeshVertex vertex4)
+	{
+		FMeshQuad quad = new FMeshQuad(vertex1,vertex2,vertex3,vertex4);
+		AddFacet(quad);
+		return quad;
+	}
+
+	public FMeshQuad GetQuad(int index)
+	{
+		return facets[index] as FMeshQuad;
+	}
+
+	//call MarkChanged any time you change the mesh!
+	public void MarkChanged()
+	{
+		version++; //things that use meshdata will check the version
+		if(SignalUpdate != null) SignalUpdate();
+	}
+}
+
+public class FMeshFacet
+{
+	public FFacetType facetType;
+
+	public FMeshVertex[] vertices;
+
+	public FMeshFacet SetVertex(int index, float x, float y, float u, float v)
+	{
+		FMeshVertex vertex = vertices[index];
+
+		vertex.x = x;
+		vertex.y = y;
+		vertex.u = u;
+		vertex.v = v;
+		
+		return this; //for chaining
+	}
+
+	public FMeshFacet SetVertex(int index, float x, float y, float u, float v, Color color)
+	{
+		FMeshVertex vertex = vertices[index];
+
+		vertex.x = x;
+		vertex.y = y;
+		vertex.u = u;
+		vertex.v = v;
+		vertex.color = color;
+		
+		return this; //for chaining
+	}
+
+	public FMeshFacet SetVertexPos(int index, float x, float y)
+	{
+		vertices[index].x = x;
+		vertices[index].y = y;
+		return this; //for chaining
+	}
+
+	public FMeshFacet SetVertexPos(int index, Vector2 pos)
+	{
+		vertices[index].x = pos.x;
+		vertices[index].y = pos.y;
+		return this; //for chaining
+	}
+
+	public FMeshFacet SetVertexUV(int index, float u, float v)
+	{
+		vertices[index].u = u;
+		vertices[index].v = v;
+		return this; //for chaining
+	}
+
+	public FMeshFacet SetVertexUV(int index, Vector2 uv)
+	{
+		vertices[index].u = uv.x;
+		vertices[index].v = uv.y;
+		return this; //for chaining
+	}
+
+	public FMeshFacet SetVertexColor(int index, Color color)
+	{
+		vertices[index].color = color;
+
+		return this; //for chaining
+	}
+
+	public FMeshFacet SetColorForAllVertices(Color color)
+	{
+		int count = vertices.Length;
+
+		for(int v = 0; v<count; v++)
+		{
+			vertices[v].color = color;	
+		}
+
+		return this; //for chaining
+	}
+
+	public FMeshFacet OffsetPos(float offsetX, float offsetY)
+	{
+		int count = vertices.Length;
+		
+		for(int v = 0; v<count; v++)
+		{
+			vertices[v].x += offsetX;
+			vertices[v].y += offsetY;	
+		}
+		
+		return this; //for chaining
+	}
+}
+
+public class FMeshQuad : FMeshFacet
+{
+	public FMeshQuad()
+	{
+		facetType = FFacetType.Quad;
+		vertices = new FMeshVertex[] {new FMeshVertex(),new FMeshVertex(),new FMeshVertex(),new FMeshVertex()};
+	}
+
+	public FMeshQuad(FMeshVertex[] vertices)
+	{
+		facetType = FFacetType.Quad;
+		this.vertices = vertices;
+	}
+
+	public FMeshQuad(FMeshVertex vertex1, FMeshVertex vertex2, FMeshVertex vertex3, FMeshVertex vertex4)
+	{
+		facetType = FFacetType.Quad;
+		vertices = new FMeshVertex[] {vertex1,vertex2,vertex3,vertex4};
+	}
+
+	public FMeshQuad SetPosRect(float leftX, float bottomY, float width, float height)
+	{
+		vertices[0].SetPos(leftX,bottomY+height);
+		vertices[1].SetPos(leftX+width,bottomY+height);
+		vertices[2].SetPos(leftX+width,bottomY);
+		vertices[3].SetPos(leftX,bottomY);
+		
+		return this; //for chaining
+	}
+
+	public FMeshQuad SetPosRect(Rect rect)
+	{
+		float leftX = rect.xMin;
+		float rightX = rect.xMax;
+		float bottomY = rect.yMin;
+		float topY = rect.yMax;
+		
+		vertices[0].SetPos(leftX,topY);
+		vertices[1].SetPos(rightX,topY);
+		vertices[2].SetPos(rightX,bottomY);
+		vertices[3].SetPos(leftX,bottomY);
+		
+		return this; //for chaining
+	}
+
+	public FMeshQuad SetUVRect(float leftX, float bottomY, float width, float height)
+	{
+		vertices[0].SetUV(leftX,bottomY+height);
+		vertices[1].SetUV(leftX+width,bottomY+height);
+		vertices[2].SetUV(leftX+width,bottomY);
+		vertices[3].SetUV(leftX,bottomY);
+		
+		return this; //for chaining
+	}
+
+	public FMeshQuad SetUVRect(Rect rect)
+	{
+		float leftX = rect.xMin;
+		float rightX = rect.xMax;
+		float bottomY = rect.yMin;
+		float topY = rect.yMax;
+		
+		vertices[0].SetUV(leftX,topY);
+		vertices[1].SetUV(rightX,topY);
+		vertices[2].SetUV(rightX,bottomY);
+		vertices[3].SetUV(leftX,bottomY);
+		
+		return this; //for chaining
+	}
+
+	public FMeshQuad SetUVRectFull() //creates a uv rect that takes up the whole element texture
+	{
+		vertices[0].SetUV(0.0f,1.0f);
+		vertices[1].SetUV(1.0f,1.0f);
+		vertices[2].SetUV(1.0f,0.0f);
+		vertices[3].SetUV(0.0f,0.0f);
+		
+		return this; //for chaining
+	}
+
+	public FMeshQuad SetUVRectFromElement(FAtlasElement element) //creates a uv rect that takes up the whole element texture
+	{
+		float leftX = element.uvRect.xMin;
+		float rightX = element.uvRect.xMax;
+		float bottomY = element.uvRect.yMin;
+		float topY = element.uvRect.yMax;
+
+		vertices[0].SetUV(leftX,topY);
+		vertices[1].SetUV(rightX,topY);
+		vertices[2].SetUV(rightX,bottomY);
+		vertices[3].SetUV(leftX,bottomY);
+		
+		return this; //for chaining
+	}
+}
+
+public class FMeshTriangle : FMeshFacet
+{
+	public FMeshTriangle()
+	{
+		facetType = FFacetType.Triangle;
+		vertices = new FMeshVertex[] {new FMeshVertex(),new FMeshVertex(),new FMeshVertex()};
+	}
+
+	public FMeshTriangle(FMeshVertex[] vertices)
+	{
+		facetType = FFacetType.Triangle;
+		this.vertices = vertices;
+	}
+	
+	public FMeshTriangle(FMeshVertex vertex1, FMeshVertex vertex2, FMeshVertex vertex3)
+	{
+		facetType = FFacetType.Triangle;
+		vertices = new FMeshVertex[] {vertex1,vertex2,vertex3};
+	}
+}
+
+
+public class FMeshVertex
+{
+	public float x;
+	public float y;
+
+	public float u;
+	public float v;
+
+	public Color color = Futile.white;
+
+	public FMeshVertex()
+	{
+
+	}
+
+	public FMeshVertex(float x, float y, float u, float v)
+	{
+		this.x = x;
+		this.y = y;
+		this.u = u;
+		this.v = v;
+	}
+
+	public FMeshVertex(Vector2 pos, Vector2 uv)
+	{
+		x = pos.x;
+		y = pos.y;
+		u = uv.x;
+		v = uv.y;
+	}
+
+	public void Set(float x, float y, float u, float v)
+	{
+		this.x = x;
+		this.y = y;
+		this.u = u;
+		this.v = u;
+	}
+
+	public void SetPos(float x, float y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	public void SetUV(float u, float v)
+	{
+		this.u = u;
+		this.v = u;
+	}
+
+	//making the getters and setters below a little bit clumsy so people prefer the direct values instead
+
+	public void SetPos(Vector2 pos)
+	{
+		x = pos.x;
+		y = pos.y;
+	}
+
+	public void SetUV(Vector2 uv)
+	{
+		u = uv.x;
+		v = uv.y;
+	}
+
+	public Vector2 GetPos()
+	{
+		return new Vector2(x,y);
+	}
+
+	public Vector2 GetUV()
+	{
+		return new Vector2(u,v);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ FutileProject/Assets/Futile/Display/MeshNodes/FMeshNode.cs
@@ -0,0 +1,286 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class FMeshNode : FFacetElementNode
+{
+	protected Color _color = Futile.white;
+	protected Color _alphaColor = Futile.white;
+	
+	protected bool _isMeshDirty = false;
+
+	protected FMeshData _meshData;
+	protected int _previousMeshDataVersion;
+
+	protected float _uvScaleX;
+	protected float _uvScaleY;
+
+	protected float _uvOffsetX;
+	protected float _uvOffsetY;
+
+	protected bool _shouldUseWholeAtlas = false;
+
+	protected FMeshNode() : base() //for overriding
+	{
+	}
+
+	public FMeshNode (FFacetType facetType, string elementName) : this(new FMeshData(facetType), Futile.atlasManager.GetElementWithName(elementName))
+	{
+	}
+
+	public FMeshNode (FFacetType facetType, FAtlasElement element) : this(new FMeshData(facetType), element)
+	{
+	}
+	
+	public FMeshNode (FMeshData meshData, string elementName) : this(meshData, Futile.atlasManager.GetElementWithName(elementName))
+	{
+	}
+
+	public FMeshNode (FMeshData meshData, FAtlasElement element) : base()
+	{
+		Init(meshData, element);
+	}
+
+	protected void Init(FMeshData meshData, FAtlasElement element)
+	{
+		_meshData = meshData;
+		_previousMeshDataVersion = _meshData.version;
+		
+		Init(_meshData.facetType, element,meshData.facets.Count);
+
+		_isMeshDirty = true;
+		_isAlphaDirty = true;
+	}
+
+	public override void HandleAddedToStage()
+	{
+		base.HandleAddedToStage();
+
+		if(_previousMeshDataVersion < _meshData.version) 
+		{
+			HandleMeshDataChanged();
+		}
+
+		_meshData.SignalUpdate += HandleMeshDataChanged;
+	}
+
+	public override void HandleRemovedFromStage()
+	{
+		base.HandleRemovedFromStage();
+
+		_meshData.SignalUpdate -= HandleMeshDataChanged;
+	}
+
+	public void HandleMeshDataChanged()
+	{
+		_previousMeshDataVersion = _meshData.version;
+		_isMeshDirty = true;
+
+		int facetCount = _meshData.facets.Count;
+		if(_numberOfFacetsNeeded != facetCount)
+		{
+			_numberOfFacetsNeeded = facetCount;
+			_stage.HandleFacetsChanged();
+		}
+	}
+	
+	override public void HandleElementChanged()
+	{
+		_isMeshDirty = true;
+
+		if(_shouldUseWholeAtlas)
+		{
+			_uvScaleX = 1.0f;
+			_uvScaleY = 1.0f;
+			_uvOffsetX = 0.0f;
+			_uvOffsetY = 0.0f;
+		}
+		else 
+		{
+			_uvScaleX = _element.uvRect.width;
+			_uvScaleY = _element.uvRect.height;
+			_uvOffsetX = _element.uvRect.xMin;
+			_uvOffsetY = _element.uvRect.yMin;
+		}
+	}
+	
+	override public void Redraw(bool shouldForceDirty, bool shouldUpdateDepth)
+	{
+		bool wasMatrixDirty = _isMatrixDirty;
+		bool wasAlphaDirty = _isAlphaDirty;
+		
+		UpdateDepthMatrixAlpha(shouldForceDirty, shouldUpdateDepth);
+		
+		if(shouldUpdateDepth)
+		{
+			UpdateFacets();
+		}
+		
+		if(wasMatrixDirty || shouldForceDirty || shouldUpdateDepth)
+		{
+			_isMeshDirty = true;
+		}
+		
+		if(wasAlphaDirty || shouldForceDirty)
+		{
+			_isMeshDirty = true;
+			_color.ApplyMultipliedAlpha(ref _alphaColor, _concatenatedAlpha);	
+		}
+		
+		if(_isMeshDirty) 
+		{
+			PopulateRenderLayer();
+		}
+	}
+	
+	override public void PopulateRenderLayer()
+	{
+		if(_isOnStage && _firstFacetIndex != -1) 
+		{
+			_isMeshDirty = false;
+
+			float a = _concatenatedMatrix.a;
+			float b = _concatenatedMatrix.b;
+			float c = _concatenatedMatrix.c;
+			float d = _concatenatedMatrix.d;
+			float tx = _concatenatedMatrix.tx;
+			float ty = _concatenatedMatrix.ty;
+
+			Vector3[] vertices = _renderLayer.vertices;
+			Vector2[] uvs = _renderLayer.uvs;
+			Color[] colors = _renderLayer.colors;
+			
+			List<FMeshFacet> facets = _meshData.facets;
+			int facetCount = facets.Count;
+
+			FMeshVertex vertex;
+
+			if(_meshData.facetType == FFacetType.Triangle)
+			{
+				int vertexIndex0 = _firstFacetIndex*4;
+				int vertexIndex1 = vertexIndex0 + 1;
+				int vertexIndex2 = vertexIndex0 + 2;
+
+				for(int f = 0; f<facetCount; f++)
+				{
+					FMeshFacet facet = facets[f];
+
+					//outVector.x = localVector.x*a + localVector.y*c + tx;
+					//outVector.y = localVector.x*b + localVector.y*d + ty;
+					//outVector.z = z;
+
+					FMeshVertex[] meshVertices = facet.vertices;
+
+					vertex = meshVertices[0];
+					vertices[vertexIndex0] = new Vector3(vertex.x*a + vertex.y*c + tx,vertex.x*b + vertex.y*d + ty,_meshZ);
+					vertex = meshVertices[1];
+					vertices[vertexIndex1] = new Vector3(vertex.x*a + vertex.y*c + tx,vertex.x*b + vertex.y*d + ty,_meshZ);
+					vertex = meshVertices[2];
+					vertices[vertexIndex2] = new Vector3(vertex.x*a + vertex.y*c + tx,vertex.x*b + vertex.y*d + ty,_meshZ);
+
+					//this needs to be offset by the element uvs, so that the uvs are relative to the element (add then multiply element uv)
+					uvs[vertexIndex0] = new Vector2(_uvOffsetX + meshVertices[0].u * _uvScaleX,_uvOffsetY + meshVertices[0].v * _uvScaleY);
+					uvs[vertexIndex1] = new Vector2(_uvOffsetX + meshVertices[1].u * _uvScaleX,_uvOffsetY + meshVertices[1].v * _uvScaleY);
+					uvs[vertexIndex2] = new Vector2(_uvOffsetX + meshVertices[2].u * _uvScaleX,_uvOffsetY + meshVertices[2].v * _uvScaleY);
+
+					//could also use vertex colours here!
+					colors[vertexIndex0] = _alphaColor * meshVertices[0].color;
+					colors[vertexIndex1] = _alphaColor * meshVertices[1].color;
+					colors[vertexIndex2] = _alphaColor * meshVertices[2].color;
+
+					vertexIndex0 += 3;
+					vertexIndex1 += 3;
+					vertexIndex2 += 3;
+				}
+
+			}
+			else if(_meshData.facetType == FFacetType.Quad)
+			{
+				int vertexIndex0 = _firstFacetIndex*4;
+				int vertexIndex1 = vertexIndex0 + 1;
+				int vertexIndex2 = vertexIndex0 + 2;
+				int vertexIndex3 = vertexIndex0 + 3;
+				
+				for(int f = 0; f<facetCount; f++)
+				{
+					FMeshFacet facet = facets[f];
+					
+					FMeshVertex[] meshVertices = facet.vertices;
+					vertex = meshVertices[0];
+					vertices[vertexIndex0] = new Vector3(vertex.x*a + vertex.y*c + tx,vertex.x*b + vertex.y*d + ty,_meshZ);
+					vertex = meshVertices[1];
+					vertices[vertexIndex1] = new Vector3(vertex.x*a + vertex.y*c + tx,vertex.x*b + vertex.y*d + ty,_meshZ);
+					vertex = meshVertices[2];
+					vertices[vertexIndex2] = new Vector3(vertex.x*a + vertex.y*c + tx,vertex.x*b + vertex.y*d + ty,_meshZ);
+					vertex = meshVertices[3];
+					vertices[vertexIndex3] = new Vector3(vertex.x*a + vertex.y*c + tx,vertex.x*b + vertex.y*d + ty,_meshZ);
+
+					//this needs to be offset by the element uvs, so that the uvs are relative to the element (add then multiply element uv)
+					uvs[vertexIndex0] = new Vector2(_uvOffsetX + meshVertices[0].u * _uvScaleX,_uvOffsetY + meshVertices[0].v * _uvScaleY);
+					uvs[vertexIndex1] = new Vector2(_uvOffsetX + meshVertices[1].u * _uvScaleX,_uvOffsetY + meshVertices[1].v * _uvScaleY);
+					uvs[vertexIndex2] = new Vector2(_uvOffsetX + meshVertices[2].u * _uvScaleX,_uvOffsetY + meshVertices[2].v * _uvScaleY);
+					uvs[vertexIndex3] = new Vector2(_uvOffsetX + meshVertices[3].u * _uvScaleX,_uvOffsetY + meshVertices[3].v * _uvScaleY);
+					
+					//could also use vertex colours here!
+					colors[vertexIndex0] = _alphaColor * meshVertices[0].color;
+					colors[vertexIndex1] = _alphaColor * meshVertices[1].color;
+					colors[vertexIndex2] = _alphaColor * meshVertices[2].color;
+					colors[vertexIndex3] = _alphaColor * meshVertices[3].color;
+					
+					vertexIndex0 += 4;
+					vertexIndex1 += 4;
+					vertexIndex2 += 4;
+					vertexIndex3 += 4;
+				}
+				
+			}
+
+			_renderLayer.HandleVertsChange();
+		}
+	}
+
+	virtual public Color color 
+	{
+		get { return _color; }
+		set 
+		{ 
+			if(_color != value)
+			{
+				_color = value; 
+				_isAlphaDirty = true;
+			}
+		}
+	}
+
+	public FMeshData meshData
+	{
+		get {return _meshData;}
+		set 
+		{
+			if(_meshData != value)
+			{
+				_meshData = value;
+				_previousMeshDataVersion = _meshData.version;
+				_numberOfFacetsNeeded = _meshData.facets.Count;
+				_isMeshDirty = true;
+			}
+		}
+	}
+
+	public bool shouldUseWholeAtlas
+	{
+		get
+		{
+			return _shouldUseWholeAtlas;
+		}
+		set 
+		{
+			if(_shouldUseWholeAtlas != value)
+			{
+				_shouldUseWholeAtlas = value;
+				HandleElementChanged();
+			}
+		}
+	}
+}
+
